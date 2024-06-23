121b076bfe258e7f2e61ad5714368e41436d4742
G0los <golos@hoenir.space>
2024-06-23 17:48:45 +0200

    Change hello line #8 + bump version

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,12 +1,13 @@
+use std::ops::{Add, Sub};
 use colored::Colorize;
 use crate::VERSION;
-use std::time::{SystemTime};
+use std::time::{Duration, SystemTime};
 pub mod logo;
 
 
 
 fn get_sys_time_in_secs() -> u64 {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH.add(Duration::new(1708729200,0))) {
         Ok(n) => n.as_secs(),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
