0f019c85aaea4360fbc5803f168c5cde392085fa
G0los <golos@hoenir.space>
2024-06-23 17:37:17 +0200

    Change hello line #7 + version bump

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,15 +1,24 @@
 use colored::Colorize;
 use crate::VERSION;
-
+use std::time::{SystemTime};
 pub mod logo;
+
+
+
+fn get_sys_time_in_secs() -> u64 {
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
 
 pub fn hello(){
 
     println!(
-        "[ {} CLI   | version {:<10} | {:^10} | {:^28} ]",
+        "[ {} CLI | version {:<10} | {:^12} | {:^28} ]",
         "Hœnir".blue().bold(),
         VERSION.blue(),
-        "-",
+        get_sys_time_in_secs().to_string(),
         "(c) G0los 2024 - MIT License"
     );
 
